c16d435f58ec7d290fd3bcb8276d8f869c9e9314
yerinie0526 <yerinie@naver.com>
2020-03-12 20:47:08 +0900

    first commit test

---
+++ app/src/main/java/ddwu/mobile/final_project/a2020haniumproject/MainActivity.java
@@ -0,0 +1,16 @@
+package ddwu.mobile.final_project.a2020haniumproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //깃 허브 실험
+    }
+}
