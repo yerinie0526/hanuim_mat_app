b28c7449e44d36258da0bb575314ca67f9646ca0
krlacodusk <krlacodusk@naver.com>
2020-04-15 15:59:51 +0900

    commit kcy3

--- app/src/main/java/ddwu/mobile/final_project/a2020haniumproject/MainActivity.java
+++ app/src/main/java/ddwu/mobile/final_project/a2020haniumproject/MainActivity.java
@@ -3,6 +3,8 @@ package ddwu.mobile.final_project.a2020haniumproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +18,8 @@ public class MainActivity extends AppCompatActivity {
         //채연아...잘지냈니 그동안...(별)
 
         //아닐껄... 조만간 만나자 언니...(눈물)
+    }
+    public void onClick(View v) {
+        Toast.makeText(this, "hi", Toast.LENGTH_SHORT).show();
     }
 }
